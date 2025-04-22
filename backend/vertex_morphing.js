c93ff98879c1d53b76fabfc94641b39219ffe5a8
Tara S Pande <38411006+TaraSPande@users.noreply.github.com>
2025-04-22 15:54:23 -0700

    Add files via upload

--- backend/vertex_morphing.js
+++ backend/vertex_morphing.js
@@ -1,43 +1,4 @@
-export class Vec3 {
-	constructor(x = 0, y = 0, z = 0) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
-	}
-	
-	add(v) {
-		return new Vec3(this.x + v.x, this.y + v.y, this.z + v.z);
-	}
-	
-	sub(v) {
-		return new Vec3(this.x - v.x, this.y - v.y, this.z - v.z);
-	}
-	
-	mul(scalar) {
-		return new Vec3(this.x * scalar, this.y * scalar, this.z * scalar);
-	}
-	
-	dot(v) {
-		return this.x * v.x + this.y * v.y + this.z * v.z;
-	}
-	
-	cross(v) {
-		return new Vec3(
-			this.y * v.z - this.z * v.y,
-			this.z * v.x - this.x * v.z,
-			this.x * v.y - this.y * v.x
-		);
-	}
-	
-	magnitude() {
-		return Math.sqrt(this.x ** 2 + this.y ** 2 + this.z ** 2);
-	}
-	
-	normalize() {
-		const mag = this.magnitude();
-		return new Vec3(this.x / mag, this.y / mag, this.z / mag);
-	}
-}
+import { Vec3 } from './vertex.js';
 
 export const MeshDeformation = {
 	bezierInterpolate(start, end, t) {
